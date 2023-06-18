10fc08135e3f949a7bbfe38b5ae37186e8f1e214
JaydenLee1116 <vv55adss@gmail.com>
2023-06-18 21:11:50 +0900

    ✨ feat: 메인 아이템 목록 get 로직 구현
    - 페이지네이션 추가

--- server.ts
+++ server.ts
@@ -30,6 +30,27 @@ async function main() {
     res.send('Hello World!');
   });
 
+  app.get('/products', async (req, res) => {
+    const collection = db.collection('products');
+    const result = await collection.find().toArray();
+    const response = result[0];
+    const pageNum = Number(req.query.pageNum) || 1;
+    // TODO(jayden): townId, categoryId 필터링한 로직 작성하기
+    const townId = Number(req.query.townId) || 1;
+    const categoryId = Number(req.query.categoryId) || 1;
+
+    res.json({
+      ...response,
+      data: {
+        ...response.data,
+        products: response.data.products.slice(
+          (pageNum - 1) * 10,
+          pageNum * 10,
+        ),
+      },
+    });
+  });
+
   // NOTE(jayden): mongoDB 통신 예시 코드
   // app.get('/issues', async (req, res) => {
   //   const collection = db.collection('issues');
