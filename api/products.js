9cdf17c9d2b5b0dcd672816d1902facdd42f8dc5
JaydenLee1116 <vv55adss@gmail.com>
2023-06-21 16:39:05 +0900

    ♻️  refactor: products api 수정

---
+++ api/products.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const client = new MongoClient(process.env.MONGO_URL);
+    await client
+      .connect()
+      .then(() => {
+        console.log('connected to mongoDB');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    const db = client.db(process.env.DB_NAME);
+    const collection = db.collection('products');
+    const result = await collection.find().toArray();
+    const response = result[0];
+    const pageNum = Number(req.query.pageNum) || 1;
+    // TODO(jayden): townId, categoryId 필터링한 로직 작성하기
+    const townId = Number(req.query.townId) || 1;
+    const categoryId = Number(req.query.categoryId) || 1;
+
+    res.json({
+      ...response,
+      data: {
+        ...response.data,
+        products: response.data.products.slice(
+          (pageNum - 1) * 10,
+          pageNum * 10,
+        ),
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Server error');
+  }
+});
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 const practice = require('./api/practice');
+const products = require('./api/products');
+dotenv.config();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use(express.json({ extended: false }));
-
-app.use('/practice', practice);
+app.use('api/practice', practice);
+app.use('/products', products);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
