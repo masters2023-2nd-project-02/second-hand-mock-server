ea99933e3195efe1db8e56857815b0f7f572e896
JaydenLee1116 <vv55adss@gmail.com>
2023-06-21 17:08:47 +0900

    ♻️  refactor: products 로직 수정

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const app = express();
+dotenv.config();
+
 const practice = require('./api/practice');
 const products = require('./api/products');
-dotenv.config();
+
+const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('api/practice', practice);
-app.use('api/products', products);
+app.use('/api/practice', practice);
+app.use('/api/products', products);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
