879adca9744f9e40365784cd7886cbfe5d816046
JaydenLee1116 <vv55adss@gmail.com>
2023-06-23 10:48:52 +0900

    ♻️  refactor: 판매 목록 데이터 상태 타입 변경
    - 문자열에서 숫자로 변경

--- api/sales/index.js
+++ api/sales/index.js
@@ -20,22 +20,13 @@ router.get('/', async (req, res) => {
     const response = result[0];
     const pageNum = Number(req.query.pageNum) || 1;
     const status = Number(req.query.status) || 0;
-    const getStatusWord = (status) => {
-      switch (status) {
-        case 0:
-          return '판매중';
-        case 1:
-          return '예약중';
-        case 2:
-          return '판매완료';
-      }
-    };
+
     res.json({
       ...response,
       data: {
         ...response.data,
         products: response.data.products
-          .filter((product) => product.status === getStatusWord(status))
+          .filter((product) => product.status === status)
           .slice((pageNum - 1) * 10, pageNum * 10),
       },
     });
