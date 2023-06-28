07727cdb4dfcca1756410cd00afcdfd0d85a3808
JaydenLee1116 <vv55adss@gmail.com>
2023-06-28 17:44:19 +0900

    ✨ feat: 상품 상세 mock 데이터 추가 및 api 구현

--- api/detail/index.js
+++ api/detail/index.js
@@ -15,15 +15,17 @@ router.get('/:id', async (req, res) => {
         console.log(err);
       });
     const db = client.db(process.env.DB_NAME);
-    const collection = db.collection('products');
+    const collection = db.collection('detail');
     const result = await collection.find().toArray();
     const response = result[0];
     const { id } = req.params;
-
-    res.json({
-      status: 200,
-      message: `product id: ${id}`,
-    });
+    console.log(id);
+    res.json(response);
+    // TODO(jayden): id로 상품 정보 가져오기로 변경하기
+    // res.json({
+    //   status: 200,
+    //   message: `product id: ${id}`,
+    // });
   } catch (error) {
     console.error(error);
     return res.status(500).send('Server error');
